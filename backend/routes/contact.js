b0b5c442b7fb755b6b7eb2024900fd78883f5d08
moruedee-F <monruedi2006@gmail.com>
2025-05-09 16:32:32 +0700

    session 06

---
+++ backend/routes/contact.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/', (req, res) => {
+    const {fname, lname, email, subject, message} = req.body
+    console.log('Content from submited', {fname, lname, email, subject, message});
+    res.status(200).json({status : "Message recieved"});
+});
+
+module.exports = router;
