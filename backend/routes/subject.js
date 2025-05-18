67d7780bbe39c21938b7ba236b1694dde697c317
moruedee-F <monruedi2006@gmail.com>
2025-05-18 23:04:19 +0700

    Assignment1

--- backend/routes/subject.js
+++ backend/routes/subject.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const subject = require('../data/contact_subject.json')
+
 router.get('/', (req, res) => {
-    res.end('{"contactSubject": ["General Enquiry","Class","Schedule","Instructor","Price","Other","Fai"]}');
+    // res.end('{"contactSubject": ["General Enquiry","Class","Schedule","Instructor","Price","Location","Other","kj"]}');
     res.json(subject);
 });
 
-module.exports = router;
-
+module.exports = router;0
